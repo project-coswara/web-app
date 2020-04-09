2550c003b45eeb81aec7b243760b3780c2b2fdd0
Anand Mohan <moghan.anand@gmail.com>
2020-04-09 13:48:06 +0530

    [collect] Added feedback box

--- projects/collect/src/app/thanks/thanks.component.ts
+++ projects/collect/src/app/thanks/thanks.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import * as firebase from 'firebase/app';
 import 'firebase/auth'
+import 'firebase/firestore'
+import {UserDataService} from "../../../../../src/app/user-data.service";
+import {FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'cs-thanks-thanks',
@@ -10,12 +13,39 @@ import 'firebase/auth'
 })
 
 export class ThanksComponent implements OnInit {
+  showFeedbackBox: boolean = true;
+  showFeedbackLoader: boolean = false;
+  userData = null;
 
-  constructor() { }
-  ngOnInit() {
+  feedbackControl = new FormControl(null, [Validators.required]);
+
+  constructor(private userDataService: UserDataService) {
+    this.userDataService.getUserData().subscribe(userData => {
+      this.userData = userData;
+    })
   }
+
+  ngOnInit() { }
 
   startSession() {
     firebase.auth().signOut().then();
+  }
+
+  sendFeedback() {
+    let thanksRoot = this;
+    this.showFeedbackLoader = true;
+    let userId = null;
+    if (this.feedbackControl.valid) {
+      if (this.userData) {
+        userId = this.userData.uid;
+      }
+      firebase.firestore().collection('FEEDBACK').add({
+        'fb': this.feedbackControl.value,
+        'uid': userId
+      }).then(()=> {
+        thanksRoot.showFeedbackBox = false;
+        thanksRoot.showFeedbackLoader = false;
+      });
+    }
   }
 }
