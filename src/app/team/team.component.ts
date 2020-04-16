fb9991ba863e37e34debc7bd8d3fdeb21598cdcc
Prashant <gillyprash29@gmail.com>
2020-04-16 15:26:23 +0530

    Edited footer

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -44,7 +44,7 @@ export class TeamComponent implements OnInit {
     'image': 'neeraj.jpeg'
   }, {
     'id': 'prashant',
-    'name': 'Prashant K',
+    'name': 'Prashant Krishnan',
     'designation': 'Research Assistant',
     'institution': 'IISc Bangalore',
     'areas': 'Speaker Recognition',
