2d4e723052bc732c48d6aecfb245d6b53aab67e8
Anand Mohan <moghan.anand@gmail.com>
2020-06-04 20:32:31 +0530

    Fixed ma-IN.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
-import { MatMenuModule } from "@angular/material/menu";
-import { MatIconModule } from "@angular/material/icon";
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatIconModule} from "@angular/material/icon";
+import {NgModule} from '@angular/core';
 
-import { AdminComponent } from './admin/admin.component';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ContentComponent } from "./content/content.component";
-import { LoginComponent } from "./login/login.component";
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { UserDataService } from "./user-data.service";
-import { HomeComponent } from './home/home.component';
-import { TermsComponent } from './terms/terms.component';
-import { AboutComponent } from './about/about.component';
-import { TeamComponent } from './team/team.component';
-import { ContactComponent } from './contact/contact.component';
-import { PermissionsComponent } from './permissions/permissions.component';
-import { MatSelectModule } from "@angular/material/select";
-import { NewsComponent } from './news/news.component';
-import { AnnotateComponent } from './annotate/annotate.component';
+import {AdminComponent} from './admin/admin.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ContentComponent} from "./content/content.component";
+import {LoginComponent} from "./login/login.component";
+import {NavbarComponent} from './navbar/navbar.component';
+import {FooterComponent} from './footer/footer.component';
+import {UserDataService} from "./user-data.service";
+import {HomeComponent} from './home/home.component';
+import {TermsComponent} from './terms/terms.component';
+import {AboutComponent} from './about/about.component';
+import {TeamComponent} from './team/team.component';
+import {ContactComponent} from './contact/contact.component';
+import {PermissionsComponent} from './permissions/permissions.component';
+import {MatSelectModule} from "@angular/material/select";
+import {NewsComponent} from './news/news.component';
+import {AnnotateComponent} from './annotate/annotate.component';
 import {MatRadioModule} from "@angular/material/radio";
 import {ReactiveFormsModule} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
-import { LBComponent } from './lb/lb.component';
+import {LBComponent} from './lb/lb.component';
 import {MatTableModule} from "@angular/material/table";
+import {MatDialogModule} from "@angular/material/dialog";
+import {HttpClient} from "@angular/common/http";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -45,7 +53,7 @@ import {MatTableModule} from "@angular/material/table";
     AdminComponent,
     NewsComponent,
     AnnotateComponent,
-    LBComponent
+    LBComponent,
   ],
   imports: [
     BrowserAnimationsModule,
@@ -59,16 +67,27 @@ import {MatTableModule} from "@angular/material/table";
     MatRadioModule,
     ReactiveFormsModule,
     MatInputModule,
-    MatTableModule
+    MatTableModule,
+    MatDialogModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+      defaultLanguage: 'en-US'
+    })
   ],
   providers: [
-      UserDataService
+    UserDataService
   ],
   exports: [
     ContentComponent,
     NavbarComponent,
     FooterComponent
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: []
 })
-export class AppModule { }
+export class AppModule {
+}
